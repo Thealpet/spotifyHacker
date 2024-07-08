94ced888225d10826ed88a9116aac5b7099b4d2e
bruker <thealp@altiboxmail.no>
2024-07-08 22:02:59 +0200

    change auth

--- src/auth.ts
+++ src/auth.ts
@@ -14,17 +14,25 @@ interface SpotifyToken {
   access_token: string;
   token_type: string;
   expires_in: string;
+  state?: string;
 }
 
 export const getTokenFromUrl = (): SpotifyToken => {
-  return window.location.hash
-    .substring(1)
-    .split('&')
-    .reduce((initial: Partial<SpotifyToken>, item: string) => {
-      let parts = item.split('=');
-      (initial as any)[parts[0]] = decodeURIComponent(parts[1]);
-      return initial;
-    }, {}) as SpotifyToken;
+  const hash = window.location.hash.substring(1);
+  const tokenParams = hash.split('&').reduce((acc, param) => {
+    const [key, value] = param.split('=');
+    acc[key] = decodeURIComponent(value);
+    return acc;
+  }, {} as Record<string, string>);
+
+  const token: SpotifyToken = {
+    access_token: tokenParams['access_token'],
+    token_type: tokenParams['token_type'],
+    expires_in: tokenParams['expires_in'],
+    state: tokenParams['state'],
+  };
+
+  return token;
 };
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
